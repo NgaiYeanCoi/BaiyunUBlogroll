b47418512c51a64478a379f337541eb7dcfb78a2
NgaiYeanCoi <2232047183@stayrabbit.top>
2024-10-11 23:28:51 +0800

    Combine convertMD.py and main.py

--- main.py
+++ main.py
@@ -7,9 +7,28 @@ import feedparser
 import json
 import requests
 import xml.etree.ElementTree as ET
+import time
 
-import convertMD
-
+def convertMD():
+    """
+    将config.json文件转换成markdown
+    :return:
+    """
+    currentTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) #获取当前时间
+    jsonFilename="./config.json"
+    markdownFilename= "./summary.md"
+    with open (jsonFilename,'r' , encoding='utf-8') as f:
+        entries = json.load(f)
+    markdownContent = f"# BaiyunU Blogroll\n\n - 更新时间:{currentTime}\n\n"
+    for entry in entries:
+        markdownContent += f"## [{entry['title']}]({entry['url']})\n"
+        markdownContent += f"**作者:** {entry['author']}\n"
+        markdownContent += f"**发表时间:** {entry['published']}\n"
+        if entry['updated']:
+            markdownContent += f"**更新时间:** {entry['updated']}\n"
+            markdownContent += "\n"
+    with open(markdownFilename, 'w', encoding='utf-8') as f:
+        f.write(markdownContent)
 
 def checkWebsite(url):
     """
@@ -63,5 +82,5 @@ def main():
         print('写入完成')
 if __name__ == '__main__':
     main()
-    convertMD.main()
+    convertMD()
 
