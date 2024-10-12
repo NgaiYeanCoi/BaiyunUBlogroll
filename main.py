a1000a06a89a006cf89fe4ac5d3da1a9efb3ad4f
NgaiYeanCoi <NgaiYeanCoi@users.noreply.github.com>
2024-10-12 14:51:23 +0000

    deploy: 2787c402638704248a5773a47e01c36f2f5f7321

---
+++ main.py
@@ -0,0 +1,172 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+__author__ = 'NgaiYeanCoi'
+
+import feedparser
+import json
+import requests
+import xml.etree.ElementTree as ET
+import time
+import markdown
+from markdown.extensions.toc import TocExtension
+from bs4 import BeautifulSoup
+
+
+blogUrl = [
+        'https://blog.nyc1.xyz/atom.xml',
+        'https://blog.canyie.top/atom.xml'
+        # 在此输入可以添加更多博客的RSS链接
+    ]
+
+
+def getUrlTitle(url):
+    """
+    爬取获取网页的title
+    :param url: 网页地址
+    :return: title
+    """
+    try:
+        # 发送HTTP请求
+        response = requests.get(url)
+        # 确保请求成功
+        response.raise_for_status()
+        # 使用BeautifulSoup解析HTML
+        soup = BeautifulSoup(response.text, 'html.parser')
+        # 提取网页标题
+        title = soup.title.string if soup.title else 'No title found'
+        return title
+    except requests.RequestException as e:
+        return f"Error: {e}"
+
+def convertMDtoHTML():
+    """
+    将config.json文件转换成HTML
+    :return:
+    """
+    currentTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())  # 获取当前时间
+    jsonFilename = "./config.json"
+    htmlFilename = "./index.html"
+    with open(jsonFilename, 'r', encoding='utf-8') as f:
+        entries = json.load(f)
+    head = """
+    <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BaiyunU Blogroll</title>
+    <style type="text/css">
+       """
+    with open("./index.css", 'r', encoding='utf-8') as f:
+        css = f.read()
+    # 侧边栏内容
+    Github="""
+    <a id="Github" target="_blank" href="https://github.com/NgaiYeanCoi/BaiyunUBlogroll">
+    <span id="GithubText">GitHub</span><img id="logo-github" src="https://blogroll.njulug.org/assets/github.c12ec768.png" alt="Logo">
+    </a>
+    """
+    sidebarContent = "<div class='sidebar' style='margin:10px;'><ul>"
+    for url in blogUrl:
+        title = getUrlTitle(url)
+        path = url
+        path=path.split('/')
+        sidebarContent += f"<p class='title'>{title}</p>"
+        sidebarContent += f"<li><a target='_blank' href='https://{path[2]}'>https://{path[2]}</a></li>"
+    sidebarContent += f"{Github}</ul></div>"
+
+    # Markdown内容
+    markdownContent = f"# BaiyunU Blogroll\n\n - 更新时间:{currentTime}\n\n"
+    for entry in entries:
+        markdownContent += f"## <a href=\"{entry['url']}\" target=\"_blank\">{entry['title']}</a>\n"
+        #markdownContent += f"## [{entry['title']}]({entry['url']})\n"
+        markdownContent += f"**摘要:** {entry['summary']}\n\n"
+        markdownContent += f"**作者:** {entry['author']}\n"
+        markdownContent += f"**发表时间:** {entry['published']}\n"
+        if entry['updated']:
+            markdownContent += f"**更新时间:** {entry['updated']}\n"
+        markdownContent += "\n"
+    # 转换Markdown为HTML
+    htmlContent = markdown.markdown(markdownContent, extensions=[TocExtension(baselevel=2)])
+    # 合并CSS样式、侧边栏和HTML内容
+    finalHtmlContent = f"{head}{css}</style><div class='container'>{sidebarContent}<div class='main-content'>{htmlContent}</div></div></head>"
+
+    with open(htmlFilename, 'w', encoding='utf-8') as f:
+        f.write(finalHtmlContent)
+
+def convertMD():
+    """
+    将config.json文件转换成markdown
+    :return:
+    """
+    currentTime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime()) #获取当前时间
+    jsonFilename="./config.json"
+    markdownFilename= "./summary.md"
+    with open (jsonFilename,'r' , encoding='utf-8') as f:
+        entries = json.load(f)
+    markdownContent = f"# BaiyunU Blogroll\n\n - 更新时间:{currentTime}\n\n"
+    for entry in entries:
+        markdownContent += f"## [{entry['title']}]({entry['url']})\n"
+        markdownContent += f"**作者:** {entry['author']}\n"
+        markdownContent += f"**发表时间:** {entry['published']}\n\n"
+        markdownContent += f"**摘要:** {entry['summary']}\n"
+        if entry['updated']:
+            markdownContent += f"\n**更新时间:** {entry['updated']}\n"
+            markdownContent += "\n"
+    with open(markdownFilename, 'w', encoding='utf-8') as f:
+        f.write(markdownContent)
+
+def checkWebsite(url):
+    """
+    判断网页访问是否正常&&判断XML是否有效
+    :param url: 网页地址
+    :return:
+    """
+    response = requests.get(url)
+    try:
+        if response.status_code == 200:
+            pass
+            root=ET.fromstring(response.content)
+        else:
+            raise requests.RequestException(f"{url}请求的网站访问错误")
+    except ET.ParseError:
+        raise ET.ParseError(f"{url}XML文件解析错误")
+
+def fetchBlogEntries(blogFeedUrls):
+    """
+    聚合所有URL的RSS源
+    :param: blogFeedUrls: 传入blog的网址
+    :return: 按照updates时间排序好，返回entries列表
+    """
+    allEntries = []
+    for blogFeedUrl in blogFeedUrls:
+        print(f"正在写入{blogFeedUrl}中...")
+        feedData = feedparser.parse(blogFeedUrl) #解析RSS
+        authorName = feedData.feed.get('author', 'Unknown Author')  # 获取作者信息
+        entries = feedparser.parse(blogFeedUrl)["entries"]
+        for entry in entries:
+            summaryHtml = entry["summary"]
+            summaryText = BeautifulSoup(summaryHtml, 'html.parser').get_text(strip=True)  # 提取Summary摘要纯文本内容
+            allEntries.append({
+                "author": authorName,
+                "title": entry["title"],
+                "url": entry["link"].split("#")[0],
+                "summary": summaryText,
+                "published": entry["published"].split("T")[0],
+                "updated": entry["updated"].replace("T", " ").split(".")[0] if "updated" in entry else "",
+            })
+    return sorted(allEntries, key=lambda x: x['updated'], reverse=True)
+
+def main():
+    global blogUrl
+    for check in blogUrl:
+        checkWebsite(check)
+    jsonFilename = "./config.json"
+    entries = fetchBlogEntries(blogUrl)
+    with open(jsonFilename, "w", encoding="utf-8") as f:  #将RSS解析之后将感兴趣的内容后写入到config.json文件中
+        json.dump(entries, f, ensure_ascii=False, indent=4)
+        print('写入完成')
+
+if __name__ == '__main__':
+    main()
+    convertMD()
+    convertMDtoHTML()
+
---
+++ readme.md
@@ -0,0 +1,52 @@
+# BaiyunUBlogroll
+广东白云学院Blogroll，是一个用于聚合多个博客RSS源的Python脚本。  
+
+搭配Github WorkFlow每天北京时间0:30自动抓取指定博客的RSS源，解析并提取相关信息转换成json配置文件方便处理，后将json转换成Markdown生成HTML静态网页。  
+
+项目还在建设当中，欢迎你们提交Pull Request或Issues与我一起维护这个项目。
+
+## 配置文件
+- `index.css` 用于存储静态网页层叠样式表
+- `config.json`用于存储博客RSS链接的JSON文件
+- `summary.md`生成的Markdown格式的博客信息文档
+- `index.html`生成的静态网页文件
+
+## 添加
+
+任何广东白云学院的学子都可以欢迎你们添加进来。
+
+编辑`main.py`文件，找到blogUrl，在已有的RSS源后添加你的RSS源，最后提交 Pull Request 进行更改。  
+
+格式：
+```python
+blogUrl = [
+    'https://blog.nyc1.xyz/atom.xml',
+    'https://blog.canyie.top/atom.xml',
+#   'https://URL/atom.xml'
+#   在此输入可以添加更多博客的RSS链接,记得在最后加个,
+        
+    ]
+```
+
+## 第三方搭建  
+
+### 依赖  
+
+- 在使用前先安装第三方依赖库 。
+
+```bash
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
+```  
+
+- json
+- requests
+- xml.etree.ElementTree
+- Markdown
+- beautifulsoup4
+- feedparser
+
+## 作者
+[NgaiYeanCoi](https://github.com/NgaiYeanCoi)
+
+## 许可证
+[MIT License](https://github.com/NgaiYeanCoi/BaiyunUBlogroll/blob/master/LICENSE)
